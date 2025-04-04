ac24d58e9299a5620430b73a37494cce5582b5ed
Vinnik67 <119658123+Vinnik67@users.noreply.github.com>
2025-04-04 17:04:40 +0300

    Add files via upload

---
+++ ConsoleApplication67.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+//1
+
+double add(double a, double b) 
+{
+    return a + b;
+}
+
+double subtract(double a, double b) 
+{
+    return a - b;
+}
+
+double multiply(double a, double b) 
+{
+    return a * b;
+}
+
+double divide(double a, double b) 
+{
+    if (b != 0)
+        return a / b;
+    else 
+    {
+        std::cerr << "Ошибка: деление на ноль!\n";
+        return 0;
+    }
+}
+
+int main() 
+{
+    
+    double (*operations[4])(double, double) = { add, subtract, multiply, divide };
+
+    
+    double x, y;
+    int op;
+
+    std::cout << "Введите первое число: ";
+    std::cin >> x;
+
+    std::cout << "Введите второе число: ";
+    std::cin >> y;
+
+    std::cout << "Выберите операцию (0-сложение, 1-вычитание, 2-умножение, 3-деление): ";
+    std::cin >> op;
+
+    
+    if (op >= 0 && op < 4) 
+    {
+        double result = operations[op](x, y); 
+        std::cout << "Результат: " << result << std::endl;
+    }
+    else 
+    {
+        std::cerr << "Ошибка: некорректный выбор операции!\n";
+    }
+
+    return 0;
+}
---
+++ ConsoleApplication68.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+// 2
+void sumArrays(int* A, int* B, int* C, int size) 
+{
+    for (int i = 0; i < size; ++i) 
+    {
+        C[i] = A[i] + B[i];
+    }
+}
+
+int main() 
+{
+    const int size = 5; 
+    int A[size] = { 1, 2, 3, 4, 5 }; 
+    int B[size] = { 10, 20, 30, 40, 50 }; 
+    int C[size]; 
+
+    
+    sumArrays(A, B, C, size);
+
+    
+    std::cout << "Массив C (сумма массивов A и B): ";
+    for (int i = 0; i < size; ++i) 
+    {
+        std::cout << C[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
